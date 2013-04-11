1c59d3f20865a6b5795701c5d4d1a325ca5cb2be
Shaddy Zeineddine <shaddy@chunkofwood.com>
2013-04-10 18:18:39 -0700

    added license boiler plate to files

--- src/WerkBox/Text/Creole/ItalicText.php
+++ src/WerkBox/Text/Creole/ItalicText.php
@@ -1,7 +1,27 @@
 <?php
+/**
+ * This file is part of WerkBox.
+ *
+ * WerkBox is free software; you can redistribute it and/or modify it under the terms of version 3 of the GNU General
+ * Public License as published by the Free Software Foundation.
+ *
+ * WerkBox is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @copyright 2012 Shaddy Zeineddine
+ * @license http://www.gnu.org/licenses/gpl.txt GPL v3
+ * @link https://github.com/shaddyz/werkbox
+ */
 
-namespace Creole;
+namespace WerkBox\Text\Creole;
 
+/**
+ * @package WerkBox\Text
+ * @subpackage Creole
+ */
 class ItalicText
 {
     protected $textElements = array();
